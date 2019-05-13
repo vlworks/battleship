50d6098f491b040a0f9045ec8418623913f9e7fb
VladL <vlad169@yandex.ru>
2019-05-13 18:02:01 +0300

    first step

---
+++ battleship.js
@@ -0,0 +1,49 @@
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+while (isSunk == false){
+    guess = prompt('Готово, огонь (введите число 0-6)');
+
+    if(guess < 0 || guess > 6 || isNaN(guess)){
+        alert('Введите корректное значение!');
+    } else {
+        guesses += 1;
+
+        if(guess == location1 || guess == location2 || guess == location3){
+            hits += 1;
+            alert('Попал!');
+            if(hits == 3){
+                isSunk = true;
+                alert('Вы уничтожили корабль!');                
+            }
+        } else {
+            alert('Промах!');
+        }
+    }
+
+    if(guess == null){
+        alert('Игра окончена!');
+        isSunk = true;
+    }
+}
+
+var stats = 'Вы использовали ' + guesses + ' попыток, Ваша точность: ' + (3 / guesses);
+alert(stats);
+
+
+
+
+
+
+
+
+
+//test
+// guesses -= 1;
+// console.log('Попытки: ' + guesses);
+// console.log('Hits: ' + hits);
