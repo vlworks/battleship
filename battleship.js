fb17e4bc4907e8dc5467a48f7a0d84f3b0c75761
VladL <vlad169@yandex.ru>
2019-05-28 13:09:35 +0300

    create border

--- battleship.js
+++ battleship.js
@@ -1,40 +1,23 @@
-var randomLoc = Math.floor(Math.random() * 5);
-var location1 = randomLoc;
-var location2 = location1 + 1;
-var location3 = location2 + 1;
-var guess;
-var hits = 0;
-var guesses = 0;
-var isSunk = false;
+// строим таблицу
 
-while (isSunk == false){
-    guess = prompt('Готово, огонь (введите число 0-6)');
-
-    if(guess < 0 || guess > 6 || isNaN(guess)){
-        alert('Введите корректное значение!');
-    } else {
-        guesses += 1;
-
-        if(guess == location1 || guess == location2 || guess == location3){
-            hits += 1;
-            alert('Попал!');
-            if(hits == 3){
-                isSunk = true;
-                alert('Вы уничтожили корабль!');                
-            }
-        } else {
-            alert('Промах!');
-        }
+function createTable() {
+    var SIZE = {
+        WIDTH: 7,
+        HEIGHT: 7,
     }
-
-    if(guess == null){
-        alert('Игра окончена!');
-        isSunk = true;
+    var $table = document.querySelector('#table');
+    for(var i = 0; i < SIZE.WIDTH; i++){
+        var $row = document.createElement('tr');
+        for(var j = 0; j < SIZE.HEIGHT; j++){
+            var $coll = document.createElement('td');
+            $coll.setAttribute('id', '' + i + j);
+            $row.appendChild($coll);
+        }
+        $table.appendChild($row);
     }
 }
 
-var stats = 'Вы использовали ' + guesses + ' попыток, Ваша точность: ' + (3 / guesses);
-alert(stats);
+createTable();
 
 
 
@@ -42,9 +25,3 @@ alert(stats);
 
 
 
-
-
-//test
-// guesses -= 1;
-// console.log('Попытки: ' + guesses);
-// console.log('Hits: ' + hits);
